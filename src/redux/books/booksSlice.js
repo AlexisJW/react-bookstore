939ba2ac4b5eb7df7919db7581fd7eb95d89758c
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-28 14:42:17 -0500

    Includes a reducer that add and removes a book

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,15 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
-  total: 0,
+  bookItems: [],
   isLoading: true,
 };
 
 const bookItemsSlice = createSlice({
   name: 'book',
   initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const bookId = action.payload;
+      state = state;
+    },
+    removeBook: (state, action) => {
+      const bookId = action.payload;
+      state.bookItems = state.bookItems.filter((bookItem) => bookItem.id !== bookId);
+    }
+  }
 });
 
 export default cartSlice.reducer;
