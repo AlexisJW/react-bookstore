b5619c127c9df3dbd248a4f135aa11563b09ede6
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-28 15:11:57 -0500

    Called booksReducer and categoriesReducer inside the store

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -16,8 +16,11 @@ const bookItemsSlice = createSlice({
     removeBook: (state, action) => {
       const bookId = action.payload;
       state.bookItems = state.bookItems.filter((bookItem) => bookItem.id !== bookId);
-    }
+    },
   },
 });
 
+export const {
+  addBook, removeBook,
+} = bookItemsSlice.actions;
 export default bookItemsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    book: booksReducer,
+    categories: categoriesReducer,
+  },
 });
