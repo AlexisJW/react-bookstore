50fb397af1566bdf305288010cc461efd11dacfd
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-28 14:15:57 -0500

    import store and provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+// import store and provider
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+    <Provider store={store}>
       <Main />
+    </Provider>
     </Router>
   </React.StrictMode>,
 );
