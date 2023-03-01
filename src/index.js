167a8ae22de4ce5569f3e0aa3c8bff96f9bd9950
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-28 22:09:48 -0500

    Fixed linter error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import store and provider
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import store from './redux/store';
 import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     book: booksReducer,
     categories: categoriesReducer,
   },
 });
+export default store;
