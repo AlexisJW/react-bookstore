3b79b26950c7456957a9521affe4bf7e797cdd92
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-02 12:45:50 -0500

    Adding index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import store and provider
 import { Provider } from 'react-redux';
